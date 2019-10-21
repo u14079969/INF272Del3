2bc9ac12c80232574fc60535bd3921c9b3458889
NantesduPlessis <50372251+NantesduPlessis@users.noreply.github.com>
2019-10-22 00:57:40 +0200

    u17003483

---
+++ u17003483/INF272Del3/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using INF272Del3.Models;
+using System.Data;
+using System.Data.Entity;
+using INF272Del3.ViewModels;
+
+namespace INF272Del3.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+
+        INF272D3Entities1 db = new INF272D3Entities1();
+       // [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+             User user = db.Users.Where(zz => zz.UserEmail == username && zz.PW == password).FirstOrDefault();
+
+             if (user != null)
+             {
+
+                 return RedirectToAction("Admin", "Account");
+             }
+
+             else return RedirectToAction("Index", "Users");
+             
+
+          
+            
+        }
+
+        public ActionResult Admin()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ u17003483/INF272Del3/ViewModels/PointsViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using INF272Del3.Models;
+
+namespace INF272Del3.ViewModels
+{
+    public class PointsViewModel
+    {
+        private int mPointsAvailable;
+
+        public int MPointsAvailable
+        {
+            get
+            {
+                return mPointsAvailable;
+            }
+
+            set
+            {
+                mPointsAvailable = value;
+            }
+        }
+    }
+}
